7791c3c4ed46891e8dccea76e3d86f339d670b71
bijaychanadikaljayaprakash <bijesh.jayaprakash@circles.asia>
2023-03-13 22:08:08 +0530

    Add DI scaffolding

---
+++ app/src/main/java/com/example/mynowinandroid/NiAApp.kt
@@ -0,0 +1,10 @@
+package com.example.mynowinandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NiAApp : Application(){
+
+}
+
---
+++ app/src/main/java/com/example/mynowinandroid/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.example.mynowinandroid.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule
